253360432aad7e2ec079e174bc83b8807d83a6f2
Arnaud Coomans <arnaud.coomans@gmail.com>
2013-03-02 14:46:52 -0800

    Updated example to have concurrent operations

--- WebScraperDemo/ACViewController.m
+++ WebScraperDemo/ACViewController.m
@@ -18,30 +18,93 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    self.client = [[ACWebScrapingClient alloc] init];
+    [self singleOperation];
+    //[self concurrentOperations];
+}
+
+
+- (void)singleOperation {
     
-    ACWebScrapingOperation *webScrapingOperation =
+    self.client = [[ACWebScrapingClient alloc] init];
+    ACWebScrapingOperation *webScrapingOperation = nil;
+    
+    webScrapingOperation =
     [self.client scrapURL:[NSURL URLWithString:@"http://news.ycombinator.com/"]
                 libraries:@[
-                           @"http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"
-                           ]
+                    @"http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"
+                ]
               evaluations:@[
-                            @[
-                                @"document.getElementsByTagName('a')[11].click();",
-                                @"return (document.getElementsByTagName('a')[11] ? true : false);"
-                            ],
-                            @[
-                                @"return $('span.comment font')[0].innerHTML",
-                                @"return ((typeof jQuery != 'undefined') && document.URL.substring(0, 36) == 'http://news.ycombinator.com/item?id=' ? true : false);"
-                            ],
-                           ]
+                    @[
+                        @"document.getElementsByTagName('a')[11].click();",
+                        @"return (document.getElementsByTagName('a')[11] ? true : false);"
+                    ],
+                    @[
+                        @"return $('span.comment font')[0].innerHTML",
+                        @"return ((typeof jQuery != 'undefined') && document.URL.substring(0, 36) == 'http://news.ycombinator.com/item?id=' ? true : false);"
+                    ],
+                ]
                      done:^(NSString *result) {
-                         NSLog(@"result = %@", result);
+                         NSLog(@"hacker news result = %@", result);
                      }];
     
     // let's add the webview to this controller's view so we can see what's happening; only for debugging
     [self.view addSubview:webScrapingOperation.webScraperQueue.webScraper.webview];
     webScrapingOperation.webScraperQueue.webScraper.webview.frame = self.view.bounds;
+}
+
+
+- (void)concurrentOperations {
+    CGRect frame = self.view.bounds;
+    frame.size.height /= 2;
+    
+    self.client = [[ACWebScrapingClient alloc] init];
+    ACWebScrapingOperation *webScrapingOperation = nil;
+    
+    webScrapingOperation =
+    [self.client scrapURL:[NSURL URLWithString:@"http://news.ycombinator.com/"]
+                libraries:@[
+                        @"http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"
+                ]
+              evaluations:@[
+                    @[
+                        @"document.getElementsByTagName('a')[11].click();",
+                        @"return (document.getElementsByTagName('a')[11] ? true : false);"
+                    ],
+                    @[
+                        @"return $('span.comment font')[0].innerHTML",
+                        @"return ((typeof jQuery != 'undefined') && document.URL.substring(0, 36) == 'http://news.ycombinator.com/item?id=' ? true : false);"
+                    ],
+                ]
+                     done:^(NSString *result) {
+                         NSLog(@"hacker news result = %@", result);
+                     }];
+    
+    // let's add the webview to this controller's view so we can see what's happening; only for debugging
+    [self.view addSubview:webScrapingOperation.webScraperQueue.webScraper.webview];
+    webScrapingOperation.webScraperQueue.webScraper.webview.frame = frame;
+    
+    webScrapingOperation =
+    [self.client scrapURL:[NSURL URLWithString:@"http://slashdot.org/"]
+                libraries:nil
+              evaluations:@[
+                    @[
+                        @"document.getElementsByTagName('h2')[0].getElementsByTagName('a')[0].click()",
+                        @2
+                    ],
+                    @[
+                        @"document.getElementsByClassName('body')[0].getElementsByTagName('a')[0].click()",
+                        @2
+                    ],
+                    @[@"return document.URL", @1]
+                ]
+                     done:^(NSString *result) {
+                         NSLog(@"slashdot result = %@", result);
+                     }];
+    
+    // let's add the webview to this controller's view so we can see what's happening; only for debugging
+    [self.view addSubview:webScrapingOperation.webScraperQueue.webScraper.webview];
+    frame.origin.y += frame.size.height;
+    webScrapingOperation.webScraperQueue.webScraper.webview.frame = frame;
 }
 
 @end
